23b97670bab18243ba31886d10c4d93e6eabffd2
Leonardo <leonardoa.dev@gmail.com>
2025-02-08 22:28:21 -0300

    refactor(client): fix dark mode in authentication pages

--- client/src/pages/sign-in.tsx
+++ client/src/pages/sign-in.tsx
@@ -17,17 +17,17 @@ export function SignIn() {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-dvh bg-lime-300">
+      <div className="flex flex-col items-center justify-center h-dvh bg-lime-300 dark:bg-lime-500">
         <div className="flex flex-col justify-between w-full p-6 h-56 gap-4 lg:h-auto max-w-lg lg:flex-row-reverse lg:items-center">
           <img src={Logo} height={52} width={52} className="mx-auto brightness-0 opacity-80" />
 
           <div className="lg:flex-1">
-            <h1 className="text-4xl font-black font-display">Entrar</h1>
-            <p className="font-medium opacity-70">Acesse sua conta e veja seus treinos</p>
+            <h1 className="text-4xl font-black font-display text-slate-950">Entrar</h1>
+            <p className="font-medium opacity-70 text-slate-800">Acesse sua conta e veja seus treinos</p>
           </div>
 
         </div>
-        <div className="bg-slate-100 shadow-md max-w-lg w-full p-8 space-y-6 lg:rounded-lg flex-1 lg:flex-0">
+        <div className="bg-slate-100 dark:bg-slate-900 shadow-md max-w-lg w-full p-8 space-y-6 lg:rounded-lg flex-1 lg:flex-0">
           <SignInForm />
           <p className="text-center text-sm opacity-60">
             não possui conta?{' '}
--- client/src/pages/sign-up.tsx
+++ client/src/pages/sign-up.tsx
@@ -17,18 +17,18 @@ export function SignUp() {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-dvh bg-lime-300">
+      <div className="flex flex-col items-center justify-center h-dvh bg-lime-300 dark:bg-lime-500">
         {/* <Dumbbell /> */}
         <div className="flex flex-col justify-between w-full p-6 h-56 lg:h-auto max-w-lg lg:flex-row-reverse lg:items-center">
           <img src={Logo} height={52} width={52} className="mx-auto brightness-0 opacity-80" />
 
           <div className="space-y-2">
-            <h1 className="text-4xl font-black font-display">Novo Usuário</h1>
-            <p className="font-medium leading-tight opacity-60">Cadastre-se para começar a acompanhar suas rotinas de treino</p>
+            <h1 className="text-4xl font-black font-display text-slate-950">Novo Usuário</h1>
+            <p className="font-medium leading-tight opacity-60 text-slate-800">Cadastre-se para começar a acompanhar suas rotinas de treino</p>
           </div>
 
         </div>
-        <div className="bg-slate-100 shadow-md max-w-lg w-full p-8 space-y-6 lg:rounded-lg flex-1 lg:flex-0">
+        <div className="bg-slate-100 dark:bg-slate-900 shadow-md max-w-lg w-full p-8 space-y-6 lg:rounded-lg flex-1 lg:flex-0">
           <SignUpForm />
         </div>
       </div>
